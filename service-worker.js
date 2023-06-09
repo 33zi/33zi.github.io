dc0fdfd5f5e36400bb0473ec94b5903d601fd3e1
33zi <135862142+33zi@users.noreply.github.com>
2023-06-09 09:50:40 +0200

    cache

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/encode.html',
+        'favicon.png'
+        // Add more files to cache as needed
+      ]);
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
